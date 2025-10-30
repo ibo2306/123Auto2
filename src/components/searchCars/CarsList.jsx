627f864ce4445df310f01171534ce242acedd7b0
ibo2306 <ibrahimseta@gmail.com>
2025-10-30 21:18:36 +0100

    Kategorieauswahl erstellt

--- src/components/searchCars/CarsList.jsx
+++ src/components/searchCars/CarsList.jsx
@@ -1,17 +1,13 @@
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 const CarsList = [
-  { label: 'Audi ', year: 1994 },
-  { label: 'The father', year: 1972 },
-  { label: 'The ', year: 1974 },
-  { label: 'The Dark Knight', year: 2008 },
-  { label: '12 Angry Men', year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: 'Pulp Fiction', year: 1994 },
-  {
-    label: 'The Return of the King',
-    year: 2003,
-  },
-  { label: 'The Good, the Bad and the Ugly', year: 1966 },
+  { label: 'Audi '},
+  { label: 'BMW ' },
+  { label: 'Mercedes '},
+  { label: 'Skoda' },
+  { label: 'Volkswagen' },
+  { label: "Toyota" },
+  { label: 'Porsche'},
+  { label: 'Nissan'},
   { label: 'Fight Club', year: 1999 },
   {
     label: 'The Fellowship of the Ring',
